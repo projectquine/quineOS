7e800f00eaa07470fb26a28d476aae364c027abe
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-05-04 15:18:31 +0200

    remove jykell docs

--- docs/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: default
-title: "QuineOS"
----
-
-Index page
--- docs/local_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm   --volume="$PWD:/srv/jekyll:Z"   --publish [::1]:4000:4000   jekyll/minimal   jekyll serve
